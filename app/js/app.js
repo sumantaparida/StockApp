8b3a42db409f5030ba5e605d9c42ede8abbdb5aa
Sumanta Parida <sumanta.p@turtlemint.com@Sumanta-MacBook-Pro.local>
2017-04-12 10:31:40 +0530

    App

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("stockapp",[]);
-app.directive('stockdata', stockdata);
 
+app.directive('stockdata', stockdata);
 function stockdata() {
   var directive = {
          link: link,
@@ -11,31 +11,59 @@ function stockdata() {
          bindToController: true
      };
      return directive;
-
      function link(scope, element, attrs) {
-          // console.log(scope);
-          // console.log(element);
-          // console.log(attrs);
      }
    }
 
    StockMarcketData.$inject = ['$scope'];
-   function StockMarcketData($scope) {
-      var vim = this;
-      console.log(vim.name);
-      var exampleStocks = new WebSocket("ws://stocks.mnet.website/");
-      exampleStocks.onmessage = function (event) {
-        var stockcollection = [];
-        stockcollection.push(JSON.parse(event.data));
-        //console.log(stockcollection);
-        stockcollection.forEach(function(ele,idx,arr){
-          ele.forEach(function(ele,idx,arr){
-            //console.log(ele);
-            ele.forEach(function(ele,idx,arr){
-              //$scope.name = "sumanta";
-              //console.log($scope.name);
-            })
-          })
-        });
+
+  var golbalArray={};
+
+  function update(data){
+    var prevValue = golbalArray[data[0]].currValue
+    golbalArray[data[0]].currValue = data[1];
+    golbalArray[data[0]].prevValue = prevValue;
+  }
+  function insert(data){
+    var temp ={};
+    temp[data[0]]={};
+    temp[data[0]].prevValue=0;
+    temp[data[0]].name=data[0];
+    temp[data[0]].currValue=data[1];
+    golbalArray = Object.assign(golbalArray, temp);
+  }
+  function checkValue(data){
+    if(golbalArray[data]) {
+      return true;
+    }
+    return false;
+  }
+  function get(data){
+    return golbalArray[data];
+    //console.log(golbalArray[data]);
+  }
+  function dataTransfrom(data){
+    var transfromData=[];
+    data.forEach(function(ele,idx){
+      if(checkValue(ele[0])){
+        update(ele);
       }
-   }
+      else{
+        insert(ele);
+      }
+      transfromData.push(get(ele[0]));
+    })
+    return transfromData;
+  }
+  function StockMarcketData($scope) {
+   $scope.obj = {};
+    var exampleStocks = new WebSocket("ws://stocks.mnet.website/");
+    exampleStocks.onmessage = function (event) {
+      var stockcollection = [];
+      stockcollection.push(JSON.parse(event.data));
+      stockcollection.forEach(function(ele,idx,arr) {
+        $scope.obj.name = dataTransfrom(ele);
+        $scope.$apply();
+      });
+    };
+  }
